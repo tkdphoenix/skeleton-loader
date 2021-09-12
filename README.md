aeafb5c27a1be0590e4590ea71ca0344528bec9d
Joel Grissom <joelgrissom5@gmail.com>
2021-09-11 19:03:06 -0700

    Updated code to use LitElement properties and CSS mixins to make code more dynamic for various card types

--- README.md
+++ README.md
@@ -1,21 +1,18 @@
-# New Project
+# Skeleton Loader
+
+Displays a skeleton loader that is configurable for a variable number of lines, and an optional round image. This can be modified with pull requests to inlude more scenarios.
 
 > ✨ Bootstrapped with Create Snowpack App (CSA).
 
 ## Available Scripts
+| Script Name | Description |
+| --- |--- |
+| start | Starts the development server. Open http://localhost:8080 to view it in the browser. The page will reload if you make edits. You will also see any lint errors in the console. |
+| build | Builds the app for production to the `dist/` folder. It correctly bundles the app in production mode and optimizes the build for the best performance. |
+| test | For future tests |
+| format | Runs prettier formatting |
+| update | Runs updating wizard to allow devs to select when to update major / minor / patch version changes. |
 
-### npm start
-
-Runs the app in the development mode.
-Open http://localhost:8080 to view it in the browser.
-
-The page will reload if you make edits.
-You will also see any lint errors in the console.
-
-### npm run build
-
-Builds the app for production to the `dist/` folder.
-It correctly bundles the app in production mode and optimizes the build for the best performance.
 
 ## Directives
 
--- src/skeleton-loader.ts
+++ src/skeleton-loader.ts
@@ -1,32 +1,47 @@
-import { LitElement, html, css, customElement, property } from 'lit-element/lit-element'
-
-import './gradient-bar'
-import './gradient-round'
+import { LitElement, html, css, customElement, property, TemplateResult } from 'lit-element/lit-element'
 
 @customElement('skeleton-loader')
 export class SkeletonLoader extends LitElement {
+  @property({ type: Boolean }) roundImg = false
+  @property({ type: Number }) amountOfLines = 2
+
+  _bodyTextTmpl () {
+    let count: number = 0
+    const arr: [TemplateResult] = [html``]
+    for(count; count < this.amountOfLines; count += 1) {
+      arr.push(html`<p class="row skeleton-loader"></p>`)
+    }
+    return html`
+      ${arr.map(line => {
+        return html`${line}`
+      })}
+    `
+  }
+
 	render() {
 		return html`
-			<div class="prod--wrapper">
-				<div class="prod--col prod--img">
-					<img id="productImage" class="prod--img-graphic skeleton-loader" />
-				</div>
-				<div class="prod--col prod--details">
-					<div class="prod--row prod--name">
-						<span id="productName" class="prod--name-text skeleton-loader"></span>
-					</div>
-					<div class="prod--row prod--description">
-						<span id="productId" class="prod--description-text skeleton-loader"></span>
-					</div>
-				</div>
-			</div>
+			<div class="card">
+        <div class="grid${this.roundImg ? ' withImg' : ' noImg'}">
+          ${
+            this.roundImg ? html `
+              <div class="image skeleton-loader"></div>
+            `
+            : null
+          }
+          <div class="title skeleton-loader"></div>
+        </div>
+        ${this._bodyTextTmpl()}
+      </div>
 		`
 	}
 
 	static get styles() {
 		return [
 			css`
-				.skeleton-loader:empty {
+        :host {
+          display: block;
+        }
+				.skeleton-loader {
 					width: 100%;
 					height: 15px;
 					display: block;
@@ -47,61 +62,41 @@ export class SkeletonLoader extends LitElement {
 						background-position: 100% 0, 0 0; /* move highlight to right */
 					}
 				}
-				.prod--wrapper {
-					display: flex;
-					width: 95%;
-					margin: 32px 0;
-					border: 1px solid #b6b6b6;
-					border-radius: 6px;
-					padding: 22px 10px;
-					font-family: 'Calibri', 'Arial';
-				}
-				.prod--wrapper .prod--row {
-					display: flex;
-					flex-direction: row;
-				}
-				.prod--wrapper .prod--col {
-					display: flex;
-					flex-direction: column;
-				}
-				.prod--wrapper .prod--img {
-					width: 20%;
-					margin: 0 15px;
-				}
-				.prod--wrapper .prod--img .prod--img-graphic {
-					max-height: 100%;
-					height: 100%;
-					vertical-align: top;
-					max-width: 100%;
-				}
-				.prod--wrapper .prod--details {
-					width: 90%;
-					margin-left: 17px;
-				}
-				.prod--wrapper .prod--details .prod--name {
-					margin-bottom: 3px;
-					width: 85%;
-					display: block;
-					max-width: 100%;
-				}
-				.prod--wrapper .prod--details .prod--name .prod--name-para {
-					margin: 0 auto;
-				}
-				.prod--wrapper .prod--details .prod--name .prod--name-text {
-					font-weight: bold;
-					font-size: 16px;
-					line-height: 23px;
-					color: #002877;
-					height: 40px;
-				}
-				.prod--wrapper .prod--details .prod--description {
-					margin-bottom: 13px;
-				}
-				.prod--wrapper .prod--details .prod--description .prod--description-text {
-					font-size: 13px;
-					line-height: 18px;
-					color: #666666;
-				}
+        .card {
+          width: var(--skeleton-loader-card-width, 300px);
+          height: var(--skeleton-loader-card-height, 150px);
+          border: var(--skeleton-loader-card-border, 1px solid rgba(33, 33, 33, 0.15));
+          box-shadow: var(--skeleton-loader-card-box-shadow, 0 4px 10px 0 rgba(33, 33, 33, 0.2));
+          padding: var(--skeleton-loader-card-padding, 10px);
+        }
+        .grid {
+          margin: 10px 0 30px 0;
+        }
+        .grid.withImg {
+          display: grid;
+          grid-template-columns: 60px 1fr;
+        }
+        .grid.noImg {
+          display: block;
+        }
+        .image {
+          height: var(--skeleton-loader-image-height, 40px);
+          width: var(--skeleton-loader-image-width, 40px);
+          border-radius: var(--skeleton-loader-image-border-radius, 50%);
+          margin: 0 10px;
+          transform: rotate(180deg); /* for some reason, this appears to animate right to left (backwards) */
+        }
+        .title {
+          width: var(--skeleton-loader-title-width, 100%);
+          height: var(--skeleton-loader-title-height, 40px);
+          margin: var(--skeleton-loader-title-margin, 0 auto);
+        }
+        .row {
+          width: var(--skeleton-loader-row-width, 95%);
+          height: var(--skeleton-loader-row-height, 20px);
+          position: relative;
+          left: var(--skeleton-loader-row-left, 15px);
+        }
 			`,
 		]
 	}
